162c2f18f812305e9b5eca569dd17f74bb978222
toywar94 <toywar94@gmail.com>
2024-11-02 19:47:49 +0900

    chore: swagger 설정

--- src/main/kotlin/com/category/ranking/rankingservice/storeDomain/adapter/api/StoreController.kt
+++ src/main/kotlin/com/category/ranking/rankingservice/storeDomain/adapter/api/StoreController.kt
@@ -1,14 +1,21 @@
 package com.category.ranking.rankingservice.storeDomain.adapter.api
 
-import com.category.ranking.rankingservice.common.adapter.api.ApiResponse
+
+import com.category.ranking.rankingservice.common.adapter.api.CustomApiResponse
 import com.category.ranking.rankingservice.storeDomain.adapter.api.out.StoreResponse
 import com.category.ranking.rankingservice.storeDomain.service.StoreService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
+@Tag(name = "Store API", description = "상점 관련 API")
 @RestController
 @RequestMapping("/api/v1/stores")
 class StoreController(
@@ -16,15 +23,29 @@ class StoreController(
 ) {
 
 
+    @Operation(
+        summary = "위치 기반 상점 검색 API",
+        description = "위치(lat, lon)의 반경(radius)에 기반하여 상점을 검색합니다.",
+    )
+    @ApiResponses(value = [
+        ApiResponse(
+            responseCode = "200",
+            description = "검색한 상점 목록을 반환합니다."
+        ),
+        ApiResponse(
+            responseCode = "400",
+            description = "잘못된 요청"
+        )
+    ])
     @GetMapping("/search/location")
     fun searchStoresByLocation(
-        @RequestParam lat: Double,
-        @RequestParam lon: Double,
-        @RequestParam radius: Int
-    ): ResponseEntity<ApiResponse<List<StoreResponse>>> {
+        @Parameter(description = "위도", example = "37.5482359") @RequestParam lat: Double,
+        @Parameter(description = "경도", example = "126.9397607") @RequestParam lon: Double,
+        @Parameter(description = "검색 반경 (단위 : m)", example = "1000") @RequestParam radius: Int
+    ): ResponseEntity<CustomApiResponse<List<StoreResponse>>> {
 
         val stores = storeService.searchStoresByLocation(lat, lon, radius)
-        return ResponseEntity.ok(ApiResponse.success(stores))
+        return ResponseEntity.ok(CustomApiResponse.success(stores))
 
     }
 
--- src/main/kotlin/com/category/ranking/rankingservice/userDomain/adapter/api/UserController.kt
+++ src/main/kotlin/com/category/ranking/rankingservice/userDomain/adapter/api/UserController.kt
@@ -7,7 +7,9 @@ import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import com.category.ranking.rankingservice.userDomain.service.AuthService
+import io.swagger.v3.oas.annotations.tags.Tag
 
+@Tag(name = "User API", description = "유저 관련 API")
 @RestController
 @RequestMapping("/api/v1/users")
 class UserController(
