18d329c2deb971d969e63c9ee5f9b7311b9ba415
toywar94 <toywar94@gmail.com>
2024-11-23 19:15:43 +0900

    feat: 가게 리뷰 업데이트, 삭제 구현

--- src/main/kotlin/com/category/ranking/rankingservice/storeDomain/service/StoreReviewsService.kt
+++ src/main/kotlin/com/category/ranking/rankingservice/storeDomain/service/StoreReviewsService.kt
@@ -1,11 +1,14 @@
 package com.category.ranking.rankingservice.storeDomain.service
 
-import com.category.ranking.rankingservice.storeDomain.adapter.api.`in`.StoreReviewRequest
+import com.category.ranking.rankingservice.storeDomain.adapter.api.`in`.ReviewCreateRequest
+import com.category.ranking.rankingservice.storeDomain.adapter.api.`in`.ReviewUpdateRequest
 import com.category.ranking.rankingservice.storeDomain.domain.Reviews
 import com.category.ranking.rankingservice.storeDomain.domain.Status
 import com.category.ranking.rankingservice.storeDomain.repository.ReviewsJPARepository
 import jakarta.persistence.EntityNotFoundException
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class StoreReviewsService(
@@ -13,10 +16,11 @@ class StoreReviewsService(
     val storeService: StoreService
 ) {
 
-    fun saveStoreReview(uuid: String, reviewRequest: StoreReviewRequest) {
-        val userId = reviewRequest.userId
-        val rating = reviewRequest.rating
-        val content = reviewRequest.content
+    @Transactional
+    fun saveStoreReview(uuid: String, reviewCreateRequest: ReviewCreateRequest) {
+        val userId = reviewCreateRequest.userId
+        val rating = reviewCreateRequest.rating
+        val content = reviewCreateRequest.content
 
         val store = storeService.findStoreByUuid(uuid) ?: throw EntityNotFoundException("Store $uuid not found")
 
@@ -29,6 +33,29 @@ class StoreReviewsService(
                 rating
             )
         )
+    }
+
+    @Transactional
+    fun updateStoreReview(uuid: String, id: Long, reviewUpdateRequest: ReviewUpdateRequest) {
+        val rating = reviewUpdateRequest.rating
+        val content = reviewUpdateRequest.content
+
+        val review = findReviewByIdOrNull(id)
+
+        review.updateReview(review.status, content, rating)
+
+
+    }
+
+    @Transactional
+    fun softDeleteStoreReview(uuid: String, id: Long) {
+        val review = findReviewByIdOrNull(id)
+        review.updateReview(Status.DELETION, review.content, review.rating)
+    }
+
+    @Transactional(readOnly = true)
+    fun findReviewByIdOrNull(id: Long): Reviews {
+        return reviewJPARepo.findByIdOrNull(id)?: throw EntityNotFoundException("review $id not found")
     }
 
 }
